59f9875f756964fc01d0d9ec2b62e9212e0e9bff
marbwoulret <bramterlouw12@gmail.com>
2022-02-03 19:59:07 +0100

    added controllers

---
+++ app/controllers/culinarycontroller.php
@@ -0,0 +1,9 @@
+<?php
+require __DIR__ . '/controller.php';
+
+class CulinaryController extends Controller {
+    public function index() {
+        // require __DIR__ . '';
+    }
+}
+?>
---
+++ app/controllers/jazzcontroller.php
@@ -0,0 +1,9 @@
+<?php
+require __DIR__ . '/controller.php';
+
+class JazzController extends Controller {
+    public function index() {
+        // require __DIR__ . '';
+    }
+}
+?>

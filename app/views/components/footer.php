71c2e0cdbc3cecb598c204d0414d16ecf5e778f1
jessekops <70586233+jessekops@users.noreply.github.com>
2022-02-04 15:09:54 +0100

    trying to make a footer

---
+++ app/views/components/footer.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="eng">
+<head>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Untitled</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="assets/css/footer.css">
+</head>
+
+<body>
+<div class="footer-clean">
+        <footer>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col mx-1">
+                        <h3>About us</h3>
+                        <p>The Haarlem Festival is the festival of the year held in Haarlem. 
+                            Different kind of events like dance, jazz, culinary and historic 
+                            are held in and arround the city of Haarlem, the Netherlands.</p>
+                    </div>
+                    <div class="col mx-1 item social">
+                        <h3>Social media</h3>
+                        <a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-instagram"></i></a><a href="#"><i class="icon ion-social-tiktok"></i></a>
+                    </div>
+                    <div class="col mx-1">
+                        <h3>Select language</h3>
+                        <a href="#"><span class="flag-icon flag-icon-gbr"></span></a><a href="#"><span class="flag-icon flag-icon-nld"></span></a>
+                    </div>
+                    <div class="col mx-1">
+                        <h3>Overview</h3>
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Dance</a></li>
+                            <li><a href="#">Jazz</a></li>
+                            <li><a href="#">Culinary</a></li>
+                            <li><a href="#">Historic</a></li>
+                        </ul>
+                    </div>
+                    <div class="col mx-1">
+                        <h3>Contact information</h3>
+                        <ul>
+                            <li>contact@hf.com</li>
+                            <li>023-123123123</li>
+                            <li>kvk: 23312891</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -4,3 +4,7 @@ require __DIR__ . '/../components/nav-website.php';
 ?>
 <h1>Home</h1>
 </body>
+
+<?php
+require __DIR__ . '/../components/footer.php';
+?>

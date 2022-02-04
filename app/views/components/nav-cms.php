dc5bdaa518ad9d786d79dd909e1e5cf76e7358d3
marbwoulret <bramterlouw12@gmail.com>
2022-02-04 20:51:55 +0100

    structuring css

--- app/views/components/nav-cms.php
+++ app/views/components/nav-cms.php
@@ -14,13 +14,13 @@
         <!-- Unordered list of nav -->
         <ul class="cms-nav-ul">
             <li class="cms-nav-item">
-                <img class="cms-icon" src="/icons/cms-home.png" alt="home icon">
+                <img class="cms-nav-icon" src="/icons/cms-home.png" alt="home icon">
                 <a href="/cms">Home</a>
             </li>
 
             <hr>
             <li class="cms-nav-item-dance">
-                <img class="cms-icon" src="/icons/cms-arrow-down.png" alt="home icon">
+                <img class="cms-nav-icon" src="/icons/cms-arrow-down.png" alt="home icon">
                 <a id="no-pointer" href="">Dance</a>
                 <div class="cms-dropdown-content">
                     <a href="#">- Overview</a>
@@ -29,7 +29,7 @@
             </li>
 
             <li class="cms-nav-item-jazz">
-                <img class="cms-icon" src="/icons/cms-arrow-down.png" alt="home icon">
+                <img class="cms-nav-icon" src="/icons/cms-arrow-down.png" alt="home icon">
                 <a id="no-pointer" href="">Jazz</a>
                 <div class="cms-dropdown-content">
                     <a href="#">- Overview</a>
@@ -38,7 +38,7 @@
             </li>
 
             <li class="cms-nav-item-culinary">
-                <img class="cms-icon" src="/icons/cms-arrow-down.png" alt="home icon">
+                <img class="cms-nav-icon" src="/icons/cms-arrow-down.png" alt="home icon">
                 <a id="no-pointer" href="">Culinary</a>
                 <div class="cms-dropdown-content">
                     <a href="#">- Restaurants</a>
@@ -48,12 +48,12 @@
             <hr>
 
             <li class="cms-nav-item">
-                <img class="cms-icon" src="/icons/cms-new-archive.png" alt="home icon">
+                <img class="cms-nav-icon" src="/icons/cms-new-archive.png" alt="home icon">
                 <a href="#">New / Archive</a>
             </li>
             
             <li class="cms-nav-item">
-                <img class="cms-icon" src="/icons/cms-users.png" alt="home icon">
+                <img class="cms-nav-icon" src="/icons/cms-users.png" alt="home icon">
                 <a href="#">Users</a>
             </li>
         </ul>

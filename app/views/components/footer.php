0deeaf75ed0dc17eb4b3cbec959de0f8a8a0ce37
jessekops <70586233+jessekops@users.noreply.github.com>
2022-02-09 13:39:51 +0100

    trying to make css work

--- app/views/components/footer.php
+++ app/views/components/footer.php
@@ -3,56 +3,53 @@
 <head>
 <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Untitled</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-    <link rel="stylesheet" href="css/footer.css">
+    <title>Haarlm Festival</title>
+    <link rel="stylesheet" href="/public/css/footer.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
 </head>
-
 <body>
-<div class="footer-clean">
-        <footer>
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col mx-1">
-                        <h3>About us</h3>
-                        <p>The Haarlem Festival is the festival of the year held in Haarlem. 
-                            Different kind of events like dance, jazz, culinary and historic 
-                            are held in and arround the city of Haarlem, the Netherlands.</p>
-                    </div>
-                    <div class="col mx-1">
-                        <h3>Select language</h3>
-                        <a href="#"><span class="flag-icon flag-icon-gbr"></span></a><a href="#"><span class="flag-icon flag-icon-nld"></span></a>
-                    </div>
-                    <div class="col mx-1 item social">
-                        <h3>Social media</h3>
-                        <a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-instagram"></i></a><a href="#"><i class="icon ion-social-tiktok"></i></a>
-                    </div>
-                    
-                    <div class="col mx-1">
-                        <h3>Overview</h3>
-                        <ul>
-                            <li><a href="#">Home</a></li>
-                            <li><a href="#">Dance</a></li>
-                            <li><a href="#">Jazz</a></li>
-                            <li><a href="#">Culinary</a></li>
-                            <li><a href="#">Historic</a></li>
-                        </ul>
-                    </div>
-                    <div class="col mx-1">
-                        <h3>Contact information</h3>
-                        <ul>
-                            <li>contact@hf.com</li>
-                            <li>023-123123123</li>
-                            <li>kvk: 23312891</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </footer>
-    </div>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+  <footer class="footer">
+  	 <div class="container">
+  	 	<div class="row">
+  	 		<div class="footer-col">
+  	 			<h4>About us</h4>
+  	 			<p>The Haarlem Festival is the festival of the year held in Haarlem. 
+                    Different kind of events like dance, jazz, culinary and historic are held 
+                    in and arround the city of Haarlem, the Netherlands.</p>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>Select langauge</h4>
+  	 		</div>
+               <div class="footer-col">
+  	 			<h4>Socials</h4>
+  	 			<div class="social-links">
+  	 				<a href="#"><i class="fab fa-twitter"></i></a>
+  	 				<a href="#"><i class="fab fa-facebook-f"></i></a>
+  	 				<a href="#"><i class="fab fa-instagram"></i></a>
+  	 				<a href="#"><i class="fab fa-tiktok"></i></a>
+  	 			</div>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>Overview</h4>
+  	 			<ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/dance">Dance</a></li>
+                    <li><a href="/jazz">Jazz</a></li>
+                    <li><a href="/culinary">Culinary</a></li>
+                    <li><a href="/cms/login">CMS</a></li>
+                </ul>
+  	 		</div>
+               <div class="footer-col">
+  	 			<h4>Contact information</h4>
+  	 			<ul>
+  	 				<li>contact@hf.com</li>
+  	 				<li>023-123123123</li>
+  	 				<li>kvk: 23312891</li>
+                </ul>
+  	 		</div>
+  	 	</div>
+  	 </div>
+  </footer>
 </body>
 
 </html>

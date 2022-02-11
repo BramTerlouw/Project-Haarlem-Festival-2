0318ee350004dd9b2ae484d4ba4d07fe9a74b535
jessekops <70586233+jessekops@users.noreply.github.com>
2022-02-11 14:07:57 +0100

    Banners event pages done

--- app/views/Jazz/index.php
+++ app/views/Jazz/index.php
@@ -2,8 +2,24 @@
 require __DIR__ . '/../components/head.php';
 require __DIR__ . '/../components/nav-website.php';
 ?>
-<h1>Jazz</h1>
 
+<div class="Banner-jazz">
+    
+<h1 class="jazz-title">Jazz</h1>
+
+<h2 class="jazz-undertitle">Live with soul</h2>
+
+<p class="jazz-description">Haarlem Jazz is an important music event for the City of  Haarlem. 
+We want to recreate part of this festival by inviting some of the bands that
+previously performed there to play at the Patronaat. On Sunday, some of the bands will take
+to the big stage at the Grote Markt to perform for all visitors for free! 
+</p>
+
+<div class ="navbuttons-jazz">   
+<button class="button-tickets-jazz"type="button">Program & Tickets</button>
+<button class="button-venues-jazz"type="button">Venues</button>
+</div>
+</div>
 <?php
 require __DIR__ . '/../components/footer.php';
 ?>

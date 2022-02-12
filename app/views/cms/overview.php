7bb0857fbfdb2c66e823fc2398e24028668e563f
marbwoulret <bramterlouw12@gmail.com>
2022-02-12 15:05:33 +0100

    finished event page for now

--- app/views/cms/overview.php
+++ app/views/cms/overview.php
@@ -13,7 +13,7 @@ $event = $_GET['event'];
     <div class="overview-info">
         <div class="form-item">
             <label for="">Event name:</label>
-            <input type="text" name="" value="test">
+            <input type="text" name="" value="Jazz event">
             <img src="/icons/cms-edit-form.png" alt="edit button">
         </div>
         <div class="form-item-dropdwn">
@@ -27,40 +27,24 @@ $event = $_GET['event'];
         </div>
         <div class="form-item">
             <label for="">Description:</label>
-            <input type="text" name="" value="test">
-            <img src="/icons/cms-edit-form.png" alt="edit button">
+            <textarea name="" rows="6">Haarlem Jazz is an important music event for the city of Haarlem. During the Haarlem Festival, we want to recreate part of this festival.</textarea>
         </div>
-        <div class="form-item">
+        <div class="form-item lower-form-img">
             <label for="">Date:</label>
-            <input type="text" name="" value="test">
+            <input type="text" name="" value="28 July 2022">
+            <input type="text" name="" value="31 July 2022">
             <img src="/icons/cms-edit-form.png" alt="edit button">
         </div>
-        <div class="form-item">
+        <div class="form-item lower-form-img">
             <label for="">Location(s):</label>
-            <input type="text" name="" value="test">
-            <img src="/icons/cms-edit-form.png" alt="edit button">
-        </div>
-        <div class="form-item">
-            <label for="">Location(s):</label>
-            <input type="text" name="" value="test">
-            <img src="/icons/cms-edit-form.png" alt="edit button">
-        </div>
-        <div class="form-item">
-            <label for="">Location(s):</label>
-            <input type="text" name="" value="test">
-            <img src="/icons/cms-edit-form.png" alt="edit button">
-        </div>
-        <div class="form-item">
-            <label for="">Location(s):</label>
-            <input type="text" name="" value="test">
+            <p><strong>Patronaat:</strong> Main hall, Second Hall, Third Hall</p>
+            <p><strong>Grote markt:</strong> N.v.t.</p>
             <img src="/icons/cms-edit-form.png" alt="edit button">
         </div>
     </div>
 
     <div class="overview-image">
-
     </div>
-
 </section>
 
 <section class="overview-container-table">
@@ -68,7 +52,7 @@ $event = $_GET['event'];
     <div class="overview-btn-container">
 
         <ul class="overview-btns-left">
-            <li><a href="#">Thursday</a></li>
+            <li><a class="btns-left-active" href="#">Thursday</a></li>
             <li><a href="">Friday</a></li>
             <li><a href="">Saturday</a></li>
             <li><a href="">Sunday</a></li>
@@ -90,10 +74,46 @@ $event = $_GET['event'];
         </ul>
 
     </div>
-    <div class="table">
-        table
-    </div>
-
+    <div class="table-wrapper">
+        <table>
+            <tr>
+                <th></th>
+                <th>Activity name</th>
+                <th>Time</th>
+                <th>Location</th>
+                <th>Hall</th>
+                <th>Price</th>
+                <th>Edit</th>
+            </tr>
+            <tr>
+                <td><input type="checkbox" name="" id=""></td>
+                <td>Gumbo Kings</td>
+                <td>18:00 - 19:00</td>
+                <td>Patronaat</td>
+                <td>Main Hall</td>
+                <td>15.00 ,-</td>
+                <td><a href=""><img src="/icons/cms-table-edit.png" alt=""></a></td>
+            </tr>
+            <tr>
+                <td><input type="checkbox" name="" id=""></td>
+                <td>Evolve</td>
+                <td>19:30 - 20:30</td>
+                <td>Patronaat</td>
+                <td>Main Hall</td>
+                <td>15.00 ,-</td>
+                <td><a href=""><img src="/icons/cms-table-edit.png" alt=""></a></td>
+            </tr>
+            <tr>
+                <td><input type="checkbox" name="" id=""></td>
+                <td>Njtam Rosie</td>
+                <td>21:00 - 22:00</td>
+                <td>Patronaat</td>
+                <td>Main Hall</td>
+                <td>15.00 ,-</td>
+                <td><a href=""><img src="/icons/cms-table-edit.png" alt=""></a></td>
+            </tr>
+        </table>
+        </div>
 </section>
 </main> <!-- close main tag from cms nav -->
 </div> <!-- close div of whole container from cms nav -->

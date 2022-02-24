510164568bb1f89bb9f93e389836bb63dad3eeae
marbwoulret <bramterlouw12@gmail.com>
2022-02-24 15:41:59 +0100

    added orders table

--- app/views/cms/order.php
+++ app/views/cms/order.php
@@ -4,6 +4,27 @@ require __DIR__ . '/../components/navigation/nav-cms.php';
 ?>
 
 <h1>Orders:</h1>
-
+<div class="orders-table-container">
+<table class="orders-table">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Customer</th>
+            <th>PhoneNumber</th>
+            <th>Email</th>
+            <th></th>
+        </tr>
+    </thead>
+    <tbody>
+       <tr>
+           <td>1</td>
+           <td>Mark het feestbeest</td>
+           <td>062918798</td>
+           <td>feestbeest@feestje.feest</td>
+           <td><a href="">View</a></td>
+       </tr>
+    </tbody>
+</table>
+</div>
 </main> <!-- close main tag from cms nav -->
 </div> <!-- close div of whole container from cms nav -->

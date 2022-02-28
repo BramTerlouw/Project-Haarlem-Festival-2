0ed8539d0b708ecb3e4b381727eefcca2413ee9f
jessekops <70586233+jessekops@users.noreply.github.com>
2022-02-28 13:57:23 +0100

    ticket display

--- app/models/event.php
+++ app/models/event.php
@@ -1,15 +1,17 @@
 <?php
 
 class Event {
-  public int $Event_ID;
+    public int $EventItem_ID;
     public string $Name;
-    public string $StartDate;
-    public string $EndDate;
-    public string $Genre;
-    public string $Gender;
     public string $Description;
     public string $Type;
-    public string $Status;
+    public string $Date;
+    public string $Start_Time;
+    public int $Location_ID;
+    public int $Ticket_Price;
+    public string $End_Time;
+    public int $Tickets;
+    public int $Event_ID;
 
     // public function __get($property) {
     //     if (property_exists($this, $property)) {

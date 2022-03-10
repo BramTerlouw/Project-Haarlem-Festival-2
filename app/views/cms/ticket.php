e7afc38b7047158e90a32a62186064005c23ca25
marbwoulret <bramterlouw12@gmail.com>
2022-03-10 20:58:20 +0100

    start on tickets

--- app/views/cms/ticket.php
+++ app/views/cms/ticket.php
@@ -4,6 +4,16 @@ require __DIR__ . '/../components/navigation/nav-cms.php';
 ?>
 
 <h1><?php echo ucfirst($_GET['event']) ?> tickets:</h1>
+<table class="table-tickets">
+            <tr>
+                <th></th>
+                <th>Program item</th>
+                <th>Ticket Price</th>
+                <th>Total</th>
+                <th>Sold</th>
+                <th>Edit</th>
+            </tr>
+        </table>
 
 </main> <!-- close main tag from cms nav -->
 </div> <!-- close div of whole container from cms nav -->

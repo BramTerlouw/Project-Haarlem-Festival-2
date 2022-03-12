2d874ae9eb3460a02cbd60cbb76df70c74804eb9
jessekops <70586233+jessekops@users.noreply.github.com>
2022-03-12 14:58:16 +0100

    adding images and tickets to jazz

--- app/views/Jazz/index.php
+++ app/views/Jazz/index.php
@@ -20,6 +20,38 @@ to the big stage at the Grote Markt to perform for all visitors for free!
 <button class="button-venues-jazz"type="button">Venues</button>
 </div>
 </div>
+
+<div class = "ticket-overview-jazz">
+
+<div class = "ticket-navigation-jazz">
+    <h3 class="nav-tickets-year-jazz">2022</h3>
+
+    <div class ="navbuttons-tickets-jazz">   
+        <button class="button-AllAccess-jazz"type="button">All-Access</button>
+        <button class="button-Friday-jazz"type="button">FRI. 29 July</button>
+        <button class="button-Saturday-jazz"type="button">SAT. 30 July</button>
+        <button class="button-Sunday-jazz"type="button">SUN. 31 July</button>
+    </div>
+</div>
+
+<?php
+foreach($eventlist as $event){?>
+    <table class = "table-tickets-jazz">
+    <td class = "td-item-tickets-jazz"><img class = "td-item-image-tickets-jazz" src="/images/locations/<?php echo $event["Location_ID"]?>.png"></td>
+    <td class = "td-item-tickets-jazz"><?php echo $event["Name"] ?></td>
+    <td class = "td-item-tickets-jazz"><?php echo $event["Start_Time"]?></td>
+    <td class = "td-item-tickets-jazz"><?php echo $event["End_Time"] ?></td>
+    <td class = "td-item-tickets-jazz"><?php echo $event["Location_ID"] ?></td>
+    <td class = "td-item-tickets-jazz">€<?php echo $event["Ticket_Price"] ?></td>
+    <td class = "td-item-tickets-jazz">
+    <a href="#">
+                <span class="fa fa-shopping-cart"></span>
+            </a>
+            </td>
+    </table><?php
+}?>
+</div>
+
 <?php
 require __DIR__ . '/../components/footer.php';
 ?>

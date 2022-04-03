eb044046b5c5069b05dd5cce2e02815098ca222e
Jelleinholland <668419@student.inholland.nl>
2022-04-03 15:42:50 +0200

    Bezig met mollie

--- app/controllers/website/paymentcontroller.php
+++ app/controllers/website/paymentcontroller.php
@@ -49,77 +49,49 @@ class PaymentController extends Controller {
     }
 
     public function InitializeMollie(){
-        
-        $mollie = new \Mollie\Api\MollieApiClient();
-        $mollie->setApiKey("test_Ds3fz4U9vNKxzCfVvVHJT2sgW5ECD8");
-        $payment = $mollie->payments->create([
-              "amount" => [
-                    "currency" => "EUR",
-                    "value" => "10.00" // You must send the correct number of decimals, thus we enforce the use of strings
-              ],
-              "description" => "Order #12345",
-              "redirectUrl" => "/payment/confirmation",
-              "webhookUrl" => "/payment/processpayment",
-              "metadata" => [
-                    "order_id" => "12345",
-              ],
-        ]);
+        try {
+            $orderId = time();
+            $mollie = new \Mollie\Api\MollieApiClient();
+            $mollie->setApiKey("test_Ds3fz4U9vNKxzCfVvVHJT2sgW5ECD8");
+            $payment = $mollie->payments->create([
+                  "amount" => [
+                        "currency" => "EUR",
+                        "value" => "10.00" // You must send the correct number of decimals, thus we enforce the use of strings
+                  ],
+                  "description" => "Order #12345",
+                  "redirectUrl" => " https://7433-213-127-46-47.ngrok.io/hf/payment/confirmation",
+                  "webhookUrl" => "https://7433-213-127-46-47.ngrok.io/hf/payment/ProcessPayment",
+                //   "metadata" => [
+                //     "order_id" => $order->id,
+                // ],
+                  
+            ]);
+    
+            header("Location: " . $payment->getCheckoutUrl(), true, 303);
 
-        header("Location: " . $payment->getCheckoutUrl(), true, 303);
-        $this->RetrievePayment();
-    }
-    public function RetrievePayment(){
-
-        $payment = $mollie->payments->get($payment->id);
-
-        if ($payment->isPaid())
-        {
-            echo "Payment received.";
+            
+        } catch (\Mollie\Api\Exceptions\ApiException $e) {
+            echo "API call failed: " . htmlspecialchars($e->getMessage());
         }
-
+      
     }
 
     public function ProcessPayment(){
+        $mollie = new \Mollie\Api\MollieApiClient();
+        $mollie->setApiKey("test_Ds3fz4U9vNKxzCfVvVHJT2sgW5ECD8");
         try {
-           
             $payment = $mollie->payments->get($_POST["id"]);
             $orderId = $payment->metadata->order_id;
 
             if ($payment->isPaid() && ! $payment->hasRefunds() && ! $payment->hasChargebacks()) {
                 //updaten van de payment status
                 UpdatePaymentStatus($orderId);
-
-            } elseif ($payment->isOpen()) {
+            } else {
 
                 return $this->json(["Error" => "Some error Occurred!"]);
                 
-            } elseif ($payment->isPending()) {
-                /*
-                * The payment is pending.
-                */
-            } elseif ($payment->isFailed()) {
-                /*
-                * The payment has failed.
-                */
-            } elseif ($payment->isExpired()) {
-                /*
-                * The payment is expired.
-                */
-            } elseif ($payment->isCanceled()) {
-                /*
-                * The payment has been canceled.
-                */
-            } elseif ($payment->hasRefunds()) {
-                /*
-                * The payment has been (partially) refunded.
-                * The status of the payment is still "paid"
-                */
-            } elseif ($payment->hasChargebacks()) {
-                /*
-                * The payment has been (partially) charged back.
-                * The status of the payment is still "paid"
-                */
-            }
+            } 
+            
         } catch (ApiException $e) {
             echo "API call failed: " . htmlspecialchars($e->getMessage());
         }
